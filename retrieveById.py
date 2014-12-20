9818220c54e5c394b049f460abe2ede5e8b931cc
ahdoki <da2517@columbia.edu>
2014-12-21 06:31:15 +0900

    Create retrieveById.py

---
+++ retrieveById.py
@@ -0,0 +1,56 @@
+from suds.client import Client
+from suds.transport.http import HttpTransport
+import urllib2
+
+
+
+SEARCH_LITE_URL = ['http://search.isiknowledge.com/esti/wokmws/ws/WokSearchLite?wsdl',  #version 2.0
+                   'http://search.webofknowledge.com/esti/wokmws/ws/WokSearchLite?wsdl'] #version 3.0
+
+SEARCH_URL = ['http://search.isiknowledge.com/esti/wokmws/ws/WokSearch?wsdl',   #version 2.0
+            'http://search.webofknowledge.com/esti/wokmws/ws/WokSearch?wsdl'] #version 3.0
+
+class HTTPSudsPreprocessor(urllib2.BaseHandler):
+    def __init__(self, SID):
+        self.SID = SID
+
+    def http_request(self, req):
+        req.add_header('cookie', 'SID="' + self.SID + '"') #adds a header to the xml request.
+        return req
+
+    https_request = http_request
+
+def retrieve_by_id(input_term, ver, start_record, max_count, SID):
+    http = HttpTransport()
+    opener = urllib2.build_opener(HTTPSudsPreprocessor(SID))
+    http.urlopener = opener
+    client_obj = Client(SEARCH_URL[int(ver - 2)], transport = http, retxml = True)
+    rparams = {
+                'count' : max_count, # 1-100
+                'firstRecord' : start_record,
+                'sortField' : [{
+                            'name' : 'Relevance',
+                            'sort' : 'D',
+                            }]
+                }
+    cited_results = client_obj.service.retrieveById('DIIDW', input_term, 'en', rparams) #REVISE. switch the patent number lists to uids
+    #resultlist should be a result of retrievebyid method collection of pans
+    return cited_results
+
+
+def retrieve_by_id_lite(input_term, ver, start_record, max_count, SID):
+    http = HttpTransport()
+    opener = urllib2.build_opener(HTTPSudsPreprocessor(SID))
+    http.urlopener = opener
+    client_obj = Client(SEARCH_LITE_URL[int(ver - 2)], transport = http, retxml = True)
+    rparams = {
+                'count' : max_count, # 1-100
+                'firstRecord' : start_record,
+                'sortField' : [{
+                            'name' : 'Relevance',
+                            'sort' : 'D',
+                            }]
+                }
+    cited_results = client_obj.service.retrieveById('DIIDW', input_term, 'en', rparams) #REVISE. switch the patent number lists to uids
+    #resultlist should be a result of retrievebyid method collection of pans
+    return cited_results
